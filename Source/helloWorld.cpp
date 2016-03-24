15d4ca8f91b630d0c21cf352c7d6ecfbd6449b3a
dpreisser <Dietmar.Preisser@vectorcast.com>
2016-03-24 11:10:39 +0000

    Hello World in loop

--- Source/helloWorld.cpp
+++ Source/helloWorld.cpp
@@ -3,7 +3,13 @@
 
 int main() {
 
-  std::cout << "Hello, World!" << std::endl;
+  typedef uint unsigned short int;
+
+  const uint numRep = 3;
+
+  for( uint i = 0; i < numRep; ++i ) {
+    std::cout << "Hello, World!" << std::endl;
+  }
 
   return 0;
 
